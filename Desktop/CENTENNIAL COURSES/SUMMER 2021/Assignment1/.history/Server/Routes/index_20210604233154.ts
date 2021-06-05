3eacff993eb5fd32319629f075b2ac8b59938f0b
fsulta16 <fsulta16@my.centennialcollege.ca>
2021-06-05 19:35:50 -0400

    updated information and stylesheet on pages along with comments added

---
+++ Desktop/CENTENNIAL COURSES/SUMMER 2021/Assignment1/.history/Server/Routes/index_20210604233154.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+const router = express.Router();
+export default router;
+//use index view and passing title of Home
+//res.render create a page through the template, pass object through the page titled property 
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home', page: 'home' });
+});
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home', page: 'home' });
+});
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me', page: 'about' });
+});
+/* GET project page. */
+router.get('/project', function(req, res, next) {
+  res.render('index', { title: 'Projects', page: 'project' });
+});
+/* GET service page. */
+router.get('/service', function(req, res, next) {
+  res.render('index', { title: 'Services', page: 'service' });
+});
+/* GET contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact', page: 'contact' });
+});
+//module.exports = router;
